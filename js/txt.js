591c78280b612e1f7d587ce532c110f27f2b35e7
Vinícius Leví <87581689+ViniciusLeviVL@users.noreply.github.com>
2024-07-25 22:20:45 -0300

    EU VOU CASAAAAAAR

--- js/txt.js
+++ js/txt.js
@@ -206,9 +206,9 @@ const txt = {
         'Eu amo seu modo de falar',
         'Eu amo a sua letra',
         'Eu amo o som da sua risada',
-        'Eu amo a sua teimosia',
-        'Eu amo a sua persistência',
-        'Eu amo a sua barriga',
+        'Eu amo o NOSSO dia 27 🤩❤️', // 27 pra dar uma de doido 😈🤪🤪🤪
+        'Eu amo como esse é o primeiro dia de mais um ano com você 🥰',
+        'Esse fica pra amanhã que agora é hora de fazer Sophia 😈', // Eu amo como você é MINHA NOIVA 😍❤️ 
         'Eu amo os seus defeitos',
         'Eu amo seu pé',
         'Eu amo todas as partes do seu corpo',
